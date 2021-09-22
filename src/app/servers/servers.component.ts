c1c3cfa93dd74b58132ae71aca8743895c615883
Ana-Maria Schpak <anamariaschpak@gmail.com>
2021-09-22 18:35:03 +0300

    demonstrated the attribute and class declarations of a component though the selector declaration in @Component decorator

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]', --> practically, you make app-servers an attribute, so in app.component it has to be a property on an element(a div for example)
+  // selector: '.app-servers',  --> practically, you make app-servers a class, so in app.component it has to be a class on an element(a div for example)
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
